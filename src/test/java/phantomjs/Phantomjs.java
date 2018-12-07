f825008b60f2fa7786acf2b16851601c43d37d03
hatem <hatem.hatamleh@gmail.com>
2018-12-07 17:07:41 +0100

    add some cool stuff

---
+++ src/test/java/phantomjs/Phantomjs.java
@@ -0,0 +1,20 @@
+package phantomjs;
+
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+
+public class Phantomjs {
+
+	public static void main(String[] args) {
+		
+//		 WebDriver driver = new PhantomJSDriver();
+//		 driver.get("vandebron.nl");
+//		 System.out.println(driver.getCurrentUrl();
+		
+			
+		
+
+	}
+
+}
